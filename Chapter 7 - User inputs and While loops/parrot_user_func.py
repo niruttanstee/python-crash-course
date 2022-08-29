acdd86e1ec7b635dcbcc4a6209246cda7b3a7b18
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-08-29 22:17:05 +0100

    Create parrot_user_func.py

---
+++ Chapter 7 - User inputs and While loops/parrot_user_func.py
@@ -0,0 +1,12 @@
+# We'll be using the input() function.
+# The function pauses the program to wait for the user to enter some text.
+# Once Python receives the user's input, it assigns that input to a variable
+# to make it convenient to work with.
+
+# Example
+message = input("Tell me something, and I will repeat it back to you: ")
+# pauses to await user input
+print(message)
+
+# Ensure that the prompts are clear and and easy to follow so they enter the
+# correct information.
