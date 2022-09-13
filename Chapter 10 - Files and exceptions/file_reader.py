ac4c215f0c1d39d8c5e3ba8f21d7b68196b72832
Nirutt Anstee <80709800+niruttanstee@users.noreply.github.com>
2022-09-13 16:52:37 +0100

    Chapter 10 update

---
+++ Chapter 10 - Files and exceptions/file_reader.py
@@ -0,0 +1,22 @@
+# We can write a program that reads in the contents of a text file and rewrite
+# it.
+
+# Step 1 is to read the file into memory by reading the entire content or
+# reading it one line at a time.
+
+# To do any work on the file we need to open it using the open function needs
+# one argument: the name of the file we want to open.
+
+# Python then looks for the file in the directory where the program that's
+# currently running. This then returns the object of the file so we use (as)
+# we use the read() method in the second line of the program to read the
+# entire contents, when we print the value of the content we get the contents
+# of the file...
+
+# The file seems to need the entire path. This is called the absolute file
+# path.
+file_path = "Chapter 10 - Files and exceptions/pi_digits.txt"
+
+with open(file_path) as file_object:
+    contents = file_object.read()
+print(contents)
---
+++ Chapter 10 - Files and exceptions/reading_line-by_line.py
@@ -0,0 +1,21 @@
+# When we are reading a file, we'll often want to examine each line of the
+# file. We may want to read or look for certain information in the file or
+# modify the text in the file in some way.
+
+# We can use a for loop on the file object to examine each line from a file
+# one line at a time:
+
+filename = "Chapter 10 - Files and exceptions/pi_digits.txt"
+
+with open(filename) as file_object:
+    for line in file_object:
+        print(line)
+
+print("\n")
+
+# there is a blank line for every print line because there is an invisible
+# newline character at the end of each line in the text file. To eliminate
+# it we can use the rstrip() which eliminates the blank lines from the right.
+with open(filename) as file_object:
+    for line in file_object:
+        print(line.rstrip())
